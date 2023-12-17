e783c64432534342d892854ba4247e3a21f491e1
tuba evcil <tugbaevcil6060@gmail.com>
2023-12-17 15:02:28 +0300

    re-installation for vite.js

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="font-bold text-red-600">
-      <h1>hello</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Introduce from "./pages/introduce";
+import "./assets/tailwind.css";
+ReactDOM.createRoot(document.getElementById("root")).render(<Introduce />);
